10b5e2663d9e80c4db047e91f9b9f8e8d310c93a
01TB <tsitohaina.berthin@gmail.com>
2025-10-19 18:46:54 +0300

    initialisation du projet

---
+++ loi-finances/src/main/java/com/project/loi_finances/LoiFinancesApplication.java
@@ -0,0 +1,13 @@
+package com.project.loi_finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoiFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoiFinancesApplication.class, args);
+	}
+
+}
---
+++ loi-finances/src/test/java/com/project/loi_finances/LoiFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.loi_finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoiFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
