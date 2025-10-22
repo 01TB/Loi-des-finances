abd2a52012c50997cfff64461aff5f7b71a06de7
Tojooooo <tojonirina.rakotobe@yahoo.com>
2025-10-22 20:18:22 +0300

    init template

--- src/main/java/com/project/loi_finances/LoiFinancesApplication.java
+++ src/main/java/com/project/loi_finances/LoiFinancesApplication.java
@@ -1,10 +1,17 @@
 package com.project.loi_finances;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LoiFinancesApplication {
+
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoiFinancesApplication.class, args);
